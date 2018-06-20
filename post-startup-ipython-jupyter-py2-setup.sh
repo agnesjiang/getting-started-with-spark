a3a0464eb96326f974199f1860e9719cdb22478a
Marck Vaisman <marck@vaisman.us>
2018-06-20 14:21:48 -0400

    move jupyter options to screen call; add scikit-learn

--- post-startup-ipython-jupyter-py2-setup.sh
+++ post-startup-ipython-jupyter-py2-setup.sh
@@ -20,7 +20,7 @@ export PYSPARK_DRIVER_PYTHON=ipython
 # export PYSPARK_DRIVER_PYTHON_OPTS='notebook'
 export PYTHONPATH=$SPARK_HOME/python:$SPARK_HOME/python/lib:$SPARK_HOME/python/lib/py4j-src.zip
 
-sudo pip install ipython==5.4 jupyter matplotlib pandas findspark
+sudo pip install ipython==5.4 jupyter matplotlib pandas findspark scikit-learn
 
 # Additions to .bashrc
 # Remember --- Amazon default .bashrc does not have an EOL, so we need to start with a blank line
@@ -28,19 +28,10 @@ echo "" >> /home/hadoop/.bashrc
 cat  >> /home/hadoop/.bashrc << EOF
 ## ANLY 502 Additions START
 export HADOOP_HOME=/usr/lib/hadoop
-export PUBLIC_IP=`curl -s http://169.254.169.254/latest/meta-data/public-ipv4`
-export INSTANCE_ID=`curl -s http://169.254.169.254/latest/meta-data/instance-id`
-export AWS_DEFAULT_REGION=$(curl --retry 5 --silent --connect-timeout 2 http://169.254.169.254/latest/dynamic/instance-identity/document | grep region | awk -F\" '{print $4}')
-export AWS_INSTANCE_ID=$(curl --silent http://169.254.169.254/latest/meta-data/instance-id)
-export AWS_AVAILABILITY_ZONE=$(curl --silent http://169.254.169.254/latest/meta-data/placement/availability-zone)
 export JAVA_HOME=/etc/alternatives/jre
 export SPARK_HOME=/usr/lib/spark
-# export PYSPARK_DRIVER_PYTHON=ipython
-# export PYSPARK_DRIVER_PYTHON_OPTS='notebook'
 export PYTHONPATH=$SPARK_HOME/python:$SPARK_HOME/python/lib:$SPARK_HOME/python/lib/py4j-src.zip
-export PYTHONHASHSEED=42
-export PYSPARK_DRIVER_PYTHON_OPTS="notebook --no-browser --port=8888 --ip=0.0.0.0 --NotebookApp.token=''"
-# alias ipyspark="export PYSPARK_DRIVER_PYTHON=python3;pyspark"
+# export PYSPARK_DRIVER_PYTHON_OPTS="notebook --no-browser --port=8888 --ip=0.0.0.0 --NotebookApp.token=''"
 
 ## ANLY 502 Additions END
 EOF
@@ -48,7 +39,7 @@ EOF
 source /home/hadoop/.bashrc
 
 # Start screen session and detach
-screen -dm bash -c "PYSPARK_DRIVER_PYTHON=jupyter pyspark"
+screen -dm bash -c "PYSPARK_DRIVER_PYTHON_OPTS=\"notebook --no-browser --port=8888 --ip=0.0.0.0 --NotebookApp.token=''\" PYSPARK_DRIVER_PYTHON=jupyter pyspark"
 
 echo Cluster Customization completed at `date`
 
